58ea94cd21981b6f00f0e77246eefebd08b381f5
Dominik Figlak <dominik.figlak@crimsonpine.com>
2021-04-26 16:06:25 +0200

    WIP scene

---
+++ Assets/ResetOnLava.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetOnLava : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            ResetGame();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "lava")
+        {
+            ResetGame();
+        }
+    }
+    
+    private void ResetGame()
+    {
+        this.transform.position = new Vector3(59.12f, 42.04f, -4.72f);
+        //this.transform.rotation = Q
+    }
+}
